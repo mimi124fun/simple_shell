9333f254b078fc9761360e6bfdd0765f24a554f0
mimi124fun <onakrimi@gmail.com>
2023-12-08 11:15:22 -0800

    task2

--- simple_shell.c
+++ simple_shell.c
@@ -1,18 +1,7 @@
-void executecom(const char*command){
-       pid_t pid;
-       pid = fork();
-    if (pid < 0){
-     perror("fork");
-      exist(EXIT_FAILURE);
-    }else if (pid ==0){
-	if (execlp(command,command,NULL)==-1){
-	    PERROR("execlp");
-	    exit(EXIT_FAILURE);
-	}
-    }else {
-    waitpid(pid,NULL,0);
-    }
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/wait.h>
 
 void executeCommand(const char *command) {
@@ -29,25 +18,44 @@ void executeCommand(const char *command) {
         token = strtok(NULL, " ");
     }
 
-    args[argCount] = NULL; 
+    args[argCount] = NULL;
     pid = fork();
 
     if (pid < 0) {
         perror("fork");
         exit(EXIT_FAILURE);
     } else if (pid == 0) {
-      
         if (execvp(args[0], args) == -1) {
-            
             perror("execvp");
             exit(EXIT_FAILURE);
         }
     } else {
-       
+
         waitpid(pid, &status, 0);
 
-        if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+        if (WIFEXITED(status) && WEXITSTATUS(status) != 0) 
             fprintf(stderr, "./shell: %s: command not found\n", args[0]);
         }
     }
+}
+
+int main() {
+    char command[100];
+
+    while (1) {
+        printf("#cisfun$ "); 
+        if (fgets(command, sizeof(command), stdin) == NULL) {
+
+            printf("\n");
+            break;
+        }
+
+
+        command[strcspn(command, "\n")] = '\0';
+
+      
+        executeCommand(command);
+    }
+
+    return 0;
 }
